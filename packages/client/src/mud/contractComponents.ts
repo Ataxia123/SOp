66330f89436ca833322c6fc129dc389e9e19d331
Ataxia123 <vp.ca94@gmail.com>
2023-05-20 12:45:34 -0500

    more changes

--- packages/client/src/mud/contractComponents.ts
+++ packages/client/src/mud/contractComponents.ts
@@ -5,6 +5,52 @@ import { defineComponent, Type as RecsType, World } from "@latticexyz/recs";
 
 export function defineContractComponents(world: World) {
   return {
+    Movable: (() => {
+      const tableId = new TableId("", "Movable");
+      return defineComponent(
+        world,
+        {
+          value: RecsType.Boolean,
+        },
+        {
+          metadata: {
+            contractId: tableId.toHexString(),
+            tableId: tableId.toString(),
+          },
+        }
+      );
+    })(),
+    Player: (() => {
+      const tableId = new TableId("", "Player");
+      return defineComponent(
+        world,
+        {
+          value: RecsType.Boolean,
+        },
+        {
+          metadata: {
+            contractId: tableId.toHexString(),
+            tableId: tableId.toString(),
+          },
+        }
+      );
+    })(),
+    Position: (() => {
+      const tableId = new TableId("", "Position");
+      return defineComponent(
+        world,
+        {
+          x: RecsType.Number,
+          y: RecsType.Number,
+        },
+        {
+          metadata: {
+            contractId: tableId.toHexString(),
+            tableId: tableId.toString(),
+          },
+        }
+      );
+    })(),
     Players: (() => {
       const tableId = new TableId("", "Players");
       return defineComponent(
--- packages/contracts/mud.config.ts
+++ packages/contracts/mud.config.ts
@@ -2,12 +2,21 @@ import { mudConfig } from "@latticexyz/world/register";
 
 export default mudConfig({
   tables: {
+    Movable: "bool",
+    Player: "bool",
+    Position:{
+      dataStruct: false,
+      schema: {
+        x: "uint32",
+        y: "uint32",
+      },
+    },
     Players: {
       schema: {
         name: "string",
         level: "string",
       },
-    }
+    },
   },
         
 modules: [
